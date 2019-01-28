0b263894f857c5c30e5033262165f165dcfd5210
JeromeHD <jaymagolan@gmail.com>
2019-01-27 22:09:00 -0500

    spotifyworking

--- liri.js
+++ liri.js
@@ -1,15 +1,76 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-
+var fs = require("fs");
 var Spotify = require("node-spotify-api");
-
 var spotify = new Spotify(keys.spotify);
+var liriReturn = process.argv[2];
 
-spotify.search({ type: "track", query: "All the Small Things" }, function(err, data) {
-  if (err) {
-    return console.log("Error occurred: " + err);
+switch (liriReturn) {
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+
+  case "movie-this":
+    movieThis();
+    break;
+
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+
+  default:
+    console.log(
+      "\n" +
+        "Type any command after 'node liri.js': " +
+        "\n" +
+        "spotify-this-song 'any song title' " +
+        "\n" +
+        "movie-this 'any movie title' " +
+        "\n" +
+        "do-what-it-says " +
+        "\n" +
+        "use quotes for multiword titles."
+    );
+}
+function spotifyThisSong(trackName) {
+  var trackName = process.argv[3];
+  if (!trackName) {
+    trackName = "The Sign";
   }
+  songRequest = trackName;
+  spotify.search(
+    {
+      type: "track",
+      query: songRequest
+    },
+    function(err, data) {
+      if (!err) {
+        var trackInfo = data.tracks.items;
+        for (var i = 0; i < 5; i++) {
+          if (trackInfo[i] != undefined) {
+            var spotifyResults =
+              "Artist: " +
+              trackInfo[i].artists[0].name +
+              "\n" +
+              "Song: " +
+              trackInfo[i].name +
+              "\n" +
+              "Preview URL: " +
+              trackInfo[i].preview_url +
+              "\n" +
+              "Album: " +
+              trackInfo[i].album.name +
+              "\n";
 
-  console.log(data);
-});
+            console.log(spotifyResults);
+            console.log(" ");
+          }
+        }
+      } else {
+        console.log("error: " + err);
+        return;
+      }
+    }
+  );
+}
